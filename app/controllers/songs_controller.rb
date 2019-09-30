d5a5b5dfa3f75f36402ab14d220a14a669c5c5b7
Brian Simonton <bsimonton78@gmail.com>
2019-09-30 15:29:12 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,6 @@
 class SongsController < ApplicationController
    
    def index
-    @songs = Song.all
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
